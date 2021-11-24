8f5af2a1817e73901ae0d0a3c5bafeeb102ecee9
RuaQ <50855683+RuaQ@users.noreply.github.com>
2021-11-24 21:42:36 +0000

    taggle:parse with pure text / mixed text&image

--- app.py
+++ app.py
@@ -71,6 +71,7 @@ def parse():
         #if platform.system().lower() == 'windows':
             #pytesseract.pytesseract.tesseract_cmd = r'C:\Users\seanq\AppData\Local\Tesseract-OCR\tesseract.exe'
         #pytesseract.pytesseract.tesseract_cmd = r'C:\Users\seanq\AppData\Local\Tesseract-OCR\tesseract.exe'
+
         data = request.get_data()
         j_data = json.loads(data)
 
@@ -89,11 +90,11 @@ def parse():
 
 
 
-        # def get_grayscale(img):
-        # return cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        def get_grayscale(img):
+            return cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-        # def thresholding(img):
-        # return cv2.threshold(img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+        def thresholding(img):
+            return cv2.threshold(img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
 
         def image_text(prep):
             return pytesseract.image_to_string(prep)
@@ -258,7 +259,6 @@ if __name__ == '__main__':
    application.run(debug = True)
 
 
-@application.route('/api/parse',methods = ['POST'])
 
 def ocr():
     #for running local
--- application.py
+++ application.py
@@ -66,6 +66,113 @@ def checkOCR():
 @application.route('/api/parse',methods = ['POST'])
 
 def parse():
+
+    data = request.get_data()
+    j_data = json.loads(data)
+    # print("input data", j_data)
+    # -------------------------- Checking Presence ------------------
+
+     # Loading the saved model with joblib
+    presence_model = joblib.load('rf_presence_classifier.joblib')
+    presence_cv = joblib.load('presence_TfidfVectorizer.joblib')
+
+    # New dataset to predict
+    origin = pd.DataFrame(j_data)
+
+    #filter type == text
+    presence_pred = origin.loc[origin['type'] == 'text']
+
+
+    # Remove the rows where the first letter starting with ignoring characters
+    ignore_str = [',', '.', ';', '{', '}', '#', '/', '(', ')', '?']
+    presence_pred = presence_pred[~presence_pred['content'].str[0].isin(ignore_str)]
+
+    # Keep the rows where the word count is between 2 and 20 (inclusive)
+    presence_pred = presence_pred[presence_pred['content'].str.split().str.len() > 1]
+    presence_pred = presence_pred[presence_pred['content'].str.split().str.len() < 21]
+
+    # Filter out the disturibing content to be removed.
+#    str_list = ['low to high', 'high to low', 'high low', 'low high', '{', 'ships', 'ship', '®', 'details',
+#                'limited edition', 'cart is currently empty', 'in cart', 'out of stock', 'believe in',
+#                'today\'s deals', 'customer service', 'offer available', 'offers available', 'collect',
+#                '% off', 'in stock soon', 'problem', 'UTC', 'javascript', 'cookie', 'cookies', 'disclaimer','https']
+
+    str_list = ['{', 'UTC']
+    pattern = '|'.join(str_list)
+
+    presence_pred = presence_pred[~presence_pred.content.str.lower().str.contains(pattern)]
+
+    # apply the pre-trained model to the new content data
+    pre_pred_vec = presence_model.predict(presence_cv.transform(presence_pred['content']))
+
+    presence_pred['presence'] = pre_pred_vec.tolist()
+
+    # dark pattern content are those where the predicted result equals to 0.
+    dark = presence_pred.loc[presence_pred['presence'] == 0]
+
+    # get the number of presence of dark pattern
+    pre_count = dark.shape[0]
+
+    # ------------------------- Category Classification ------------------
+    if pre_count == 0:
+        return_result = {
+            "total_counts": {},
+            "items_counts": {},
+            "details": []
+        }
+    else:
+        # Loading the saved model with joblib
+        cat_model = joblib.load('lr_type_classifier.joblib')
+        cat_cv = joblib.load('type_CountVectorizer.joblib')
+
+        # mapping of the encoded dark pattern categories.
+        cat_dic = {0:'FakeActivity', 1:'FakeCountdown', 2:'FakeHighDemand', 3:'FakeLimitedTime', 4:'FakeLowStock'}
+
+        cat_slug = {0:'Fake Activity', 1:'Fake Countdown', 2:'Fake High-demand', 3:'Fake Limited-time', 4:'Fake Low-stock'}
+
+        # apply the model and the countvectorizer to the detected dark pattern content data
+        cat_pred_vec = cat_model.predict(cat_cv.transform(dark['content']))                   # Problem
+
+
+        dark['category'] = cat_pred_vec.tolist()
+
+        category_list = dark['category'].tolist()
+
+        # get the mapping of the category name and encoded category integers
+        dark['category_name'] = [cat_dic[int(category)] for category in category_list]
+
+        dark['category_name_slug'] = [cat_slug[int(category)] for category in category_list]
+
+        # reset the index of the detected dark pattern list on the webpage.
+        dark = dark.reset_index(drop=True)
+
+        return_result = {
+            "total_counts": {},
+            "items_counts": {},
+            "details": []
+        }
+        # get the list of the dark patterns detected with the frequency count
+
+        return_result['total_counts'] = pre_count
+
+        counts = dark['category_name'].value_counts()
+        for index, category_name in enumerate(counts.index.tolist()):
+            return_result["items_counts"][category_name] = int(counts.values[index])
+
+        for j in range(len(dark)):
+            return_result["details"].append({
+                "content": dark['content'][j],
+                "tag":dark['tag'][j],
+                "key": dark['key'][j],
+                "category_name": dark['category_name'][j],
+                "category_name_slug": dark['category_name_slug'][j]
+            })
+        print("return_result", return_result)
+    return Response(json.dumps(return_result), mimetype='application/json')
+
+
+
+def parse_ocr():
     def ocr():
         #for running local
         #if platform.system().lower() == 'windows':
@@ -148,7 +255,6 @@ def parse():
         return texture_detect
 
     ocr = ocr()
-
 
     data = request.get_data()
     j_data = json.loads(data)
