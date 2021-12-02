b5ac76de618144f2721abbc451dbaa744305ccce
Lan Zeng <lan.zeng1995@gmail.com>
2021-12-02 20:51:45 +0000

    fix the bug in checkDP and organised the code

--- application.py
+++ application.py
@@ -12,7 +12,7 @@ import shortuuid
 from PIL import Image
 import pytesseract
 import os
-#import cv2
+import cv2
 
 application = Flask(__name__)
 
@@ -25,34 +25,11 @@ application.add_url_rule('/', 'index', (lambda: say_hello()))
 def checkDP():
     data = request.get_data()
     j_data = json.loads(data)
-
     df = pd.DataFrame(j_data)
-
-    if df['type'][0] == "text":
-        # ------ Check the 5 pattern types -------
-
-        presence_model = joblib.load('rf_presence_classifier.joblib')
-        presence_cv = joblib.load('presence_TfidfVectorizer.joblib')
-
-        prediction = presence_model.predict(presence_cv.transform(df['content']))
-
-    elif df['type'][0] == "link" or df['type'][0] == "button":
-        # ------- Check the Confirmshaming DP --------
-        link_text = df.loc[df['type'].isin(['link', 'button'])]
-
-        # Loading the saved model with joblib
-        detection_model = joblib.load('confirm_rf_clf.joblib')
-        detection_cv = joblib.load('confirm_cv.joblib')
-
-        # apply the pre-trained confirmshaming detection model to the button / link text data
-        prediction = detection_model.predict(detection_cv.transform(link_text['content']))
-
-    else:
-        return_result = {
-            "isDarkPattern": "The content type doesn't match this detection requirement!!!"
-        }
-        return Response(json.dumps(return_result), mimetype='application/json')
-
+    # ------ Check the 5 pattern types -------
+    presence_model = joblib.load('rf_presence_classifier.joblib')
+    presence_cv = joblib.load('presence_TfidfVectorizer.joblib')
+    prediction = presence_model.predict(presence_cv.transform(df['content']))
     # -------- Check the total detection result --------
     if prediction == [0]:
         return_result = {
@@ -98,36 +75,27 @@ def parse():
         #pytesseract.pytesseract.tesseract_cmd = r'C:\Users\seanq\AppData\Local\Tesseract-OCR\tesseract.exe'
         data = request.get_data()
         j_data = json.loads(data)
-
         # get urls with type = image
         full = pd.DataFrame(j_data)
         #print(full)
         urlss = full.loc[full['type'] == 'image']
         #print(urlss)
-
         urlss.duplicated(['content'])
-
         urlss3 = urlss.drop_duplicates(['content'])
-
         urlss2 = urlss3.reset_index(drop=True)
         #print(urlss2)
         #print('work')
         urls = urlss2['content']
-
         #print(urls)
-
         def get_grayscale(img):
             return cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
         def thresholding(img):
             return cv2.threshold(img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
-
         def image_text(prep):
             return pytesseract.image_to_string(prep)
 
         # create a empty dataframe
         # df_image = pd.DataFrame(columns=["content", "tag", "key", "type"])
-
         def texture_detect(all_urls):
             a = -1
             for line in all_urls:
@@ -135,7 +103,6 @@ def parse():
                 if 'https:' not in line:
                     line = "https:" + line
                 print(line)
-
                 try:
                     # print(line)
                     r = requests.get(line)
@@ -162,24 +129,17 @@ def parse():
                     #os.remove(filename)
                     # image detection
                     # print(itext)
-
-
                     urlss2['content'][a] = itext
                     print(itext)
-
-
                 except:
                     continue
 
             urlss2["content"] = urlss2["content"].map(lambda x: x.split('\n'))
-
             urlsss = urlss2.explode("content")
             #print(urlsss)
             return urlsss
-
         texture_detect = texture_detect(urls)
         #print(texture_detect)
-
         return texture_detect
 
 
@@ -187,18 +147,13 @@ def parse():
     # ---------------------------  Check Confirmshaming DP --------------------------
 
     def confirm_shaming():
-
         # Get all the HTML data
         data = request.get_data()
         j_data = json.loads(data)
-
         # get text with type == link or type == button
         html = pd.DataFrame(j_data)
         print(html)
-
         link_text = html.loc[html['type'].isin(['link','button'])]
-
-
 
         # Loading the saved model with joblib
         detection_model = joblib.load('confirm_rf_clf.joblib')
@@ -206,22 +161,18 @@ def parse():
 
         # apply the pre-trained confirmshaming detection model to the button / link text data
         pred_vec = detection_model.predict(detection_cv.transform(link_text['content']))
-
         link_text['presence'] = pred_vec.tolist()
 
         # dark pattern content are those where the predicted result equals to 0.
         confirm_shaming = link_text.loc[link_text['presence'] == 0]
-
         confirm_shaming = confirm_shaming.reset_index(drop=True)
 
         # get the number of presence of dark pattern
         confirm_shaming_count = confirm_shaming.shape[0]
         print('Number of confirmshaming DP: ', confirm_shaming_count)
-
         return confirm_shaming
 
     confirm_shaming = confirm_shaming()
-
     confirm_count = confirm_shaming.shape[0]
 
     # ----------------------------- Get Text data for 5 DP types detection ---------------------
@@ -236,17 +187,13 @@ def parse():
     presence_model = joblib.load('rf_presence_classifier.joblib')
     presence_cv = joblib.load('presence_TfidfVectorizer.joblib')
 
-
-
     # New dataset to predict
-    #pp = pd.DataFrame(j_data)
     presence_pred = pd.DataFrame(j_data)
 
     # --------- Make OCR optional -----
     if presence_pred['is_ocr'][0] == 1:
         texture_detect = ocr()
         # filter type == text
-    #textpp = pp.loc[pp['type'] == 'text']
         textpp = presence_pred.loc[presence_pred['type'] == 'text']
         combine = [textpp, texture_detect]
         presence_pred = pd.concat(combine)
@@ -270,12 +217,10 @@ def parse():
 
     str_list = ['{', 'UTC']
     pattern = '|'.join(str_list)
-
     presence_pred = presence_pred[~presence_pred.content.str.lower().str.contains(pattern)]
 
     # apply the pre-trained model to the new content data
     pre_pred_vec = presence_model.predict(presence_cv.transform(presence_pred['content']))
-
     presence_pred['presence'] = pre_pred_vec.tolist()
 
     # dark pattern content are those where the predicted result equals to 0.
@@ -304,19 +249,15 @@ def parse():
         # apply the model and the countvectorizer to the detected dark pattern content data
         type_pred_vec = type_model.predict(type_cv.transform(dark['content']))                   # Problem
 
-
         dark['type'] = type_pred_vec.tolist()
-
         type_list = dark['type'].tolist()
 
         # get the mapping of the type name and encoded type integers
         dark['type_name'] = [type_dic[int(type)] for type in type_list]
-
         dark['type_name_slug'] = [type_slug[int(type)] for type in type_list]
 
         # reset the index of the detected dark pattern list on the webpage.
         dark = dark.reset_index(drop=True)
-
         return_result = {
             "total_counts": {},
             "items_counts": {},
@@ -325,11 +266,9 @@ def parse():
         # get the list of the dark patterns detected with the frequency count
 
         return_result['total_counts'] = pre_count + confirm_count
-
         counts = dark['type_name'].value_counts()
         for index, type_name in enumerate(counts.index.tolist()):
             return_result["items_counts"][type_name] = int(counts.values[index])
-
         for j in range(len(dark)):
             return_result["details"].append({
                 "content": dark['content'][j],
@@ -342,7 +281,6 @@ def parse():
         # ----------- Add confirmshaming DP information if there is any. ---------
         if confirm_count != 0:
             return_result["items_counts"]["Confirmshaming"] = confirm_count
-
             for j in range(len(confirm_shaming)):
                 return_result["details"].append({
                     "content": confirm_shaming['content'][j],
@@ -351,7 +289,6 @@ def parse():
                     "type_name": "Confirmshaming",
                     "type_name_slug": "Confirmshaming"
                 })
-
         print("return_result", return_result)
         # ----------------
     return Response(json.dumps(return_result), mimetype='application/json')
